691fe4d0017329e408332da28f89dbbac85198b8
Usenmfon <usenmfonuko@gmail.com>
2023-10-10 13:53:59 +0100

    added base endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return response()->json([ 'message' => 'A secure Todo api endpoints' ]);
+});
+
 
 Route::group(['prefix' => 'auth'], function(){
     Route::post('/signup', [AuthController::class, 'createAccount']);
